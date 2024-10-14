a801f68b5f9cab115da05fce1682c6e89df2478c
Xinrui <xinrui@semgrep.com>
2024-10-14 15:42:52 -0400

    add main

---
+++ main.py
@@ -0,0 +1,15 @@
+def unsafe(request):
+    # ruleid: user-exec
+    code = request.POST.get("code")
+    print("something")
+    exec(code)
+
+
+def unsafe_inline(request):
+    # ruleid: user-exec
+    exec(request.GET.get("code"))
+
+
+def unsafe_dict(request):
+    # ruleid: user-exec
+    exec(request.POST["code"])
